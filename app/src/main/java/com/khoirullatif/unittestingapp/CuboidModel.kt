1621194d0f3f16948ab4107e1270ed3614d2aefd
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-07-11 08:11:38 +0700

    Membuat kelas model

---
+++ app/src/main/java/com/khoirullatif/unittestingapp/CuboidModel.kt
@@ -0,0 +1,27 @@
+package com.khoirullatif.unittestingapp
+
+
+// Dalam MVVM ini adalah kelas Model
+class CuboidModel {
+    private var length: Double = 0.0
+    private var width: Double = 0.0
+    private var height: Double = 0.0
+
+    fun save(length: Double, width: Double, height: Double) {
+        this.length = length
+        this.width = width
+        this.height = height
+    }
+
+    fun getVolume(): Double = length * width * height
+
+    fun getSurfaceArea(): Double {
+        val lw = length * width
+        val lh = length * height
+        val wh = width * height
+
+        return 2 * (lw + lh + wh)
+    }
+
+    fun getCircumference(): Double = 4 * (length + width + height)
+}
