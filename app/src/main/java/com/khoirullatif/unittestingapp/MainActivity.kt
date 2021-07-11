844b536e7c9f42eff69c8df3f37f0edf26eb359b
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-07-11 08:45:23 +0700

    MVVM selesai

--- app/src/main/java/com/khoirullatif/unittestingapp/MainActivity.kt
+++ app/src/main/java/com/khoirullatif/unittestingapp/MainActivity.kt
@@ -2,10 +2,93 @@ package com.khoirullatif.unittestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mainViewModel: MainViewModel
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var tvResult: TextView
+    private lateinit var btnCalculateVolume: Button
+    private lateinit var btnCalculateSurfaceArea: Button
+    private lateinit var btnCalculateCircumference: Button
+    private lateinit var btnSave: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = MainViewModel(CuboidModel())
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        tvResult = findViewById(R.id.tv_result)
+        btnCalculateVolume = findViewById(R.id.btn_calculate_volume)
+        btnCalculateCircumference = findViewById(R.id.btn_calculate_circumference)
+        btnCalculateSurfaceArea = findViewById(R.id.btn_calculate_surface_area)
+        btnSave = findViewById(R.id.btn_save)
+
+        btnSave.setOnClickListener(this)
+        btnCalculateVolume.setOnClickListener(this)
+        btnCalculateCircumference.setOnClickListener(this)
+        btnCalculateSurfaceArea.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val length = edtLength.text.toString().trim()
+        val width = edtWidth.text.toString().trim()
+        val height = edtHeight.text.toString().trim()
+
+        when {
+            length.isEmpty() -> edtLength.error = "Field ini tidak boleh kosong"
+            width.isEmpty() -> edtWidth.error = "Field ini tidak boleh kosong"
+            height.isEmpty() -> edtHeight.error = "Field ini tidak boleh kosong"
+            else -> {
+                val l = length.toDouble()
+                val w = width.toDouble()
+                val h = height.toDouble()
+
+                Log.d("MainActivity", "onClick: " + p0?.id.toString())
+                when (p0?.id) {
+                    R.id.btn_calculate_circumference -> {
+                        tvResult.text = mainViewModel.getCircumference().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_surface_area -> {
+                        tvResult.text = mainViewModel.getSurfaceArea().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_volume -> {
+                        tvResult.text = mainViewModel.getVolume().toString()
+                        gone()
+                    }
+                    R.id.btn_save -> {
+                        mainViewModel.save(l, w, h)
+                        visible()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun visible() {
+        btnCalculateSurfaceArea.visibility = View.VISIBLE
+        btnCalculateCircumference.visibility = View.VISIBLE
+        btnCalculateVolume.visibility = View.VISIBLE
+        btnSave.visibility = View.GONE
+    }
+
+    private fun gone() {
+        btnCalculateVolume.visibility = View.GONE
+        btnCalculateCircumference.visibility = View.GONE
+        btnCalculateSurfaceArea.visibility = View.GONE
+        btnSave.visibility = View.VISIBLE
     }
 }
